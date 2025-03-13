e03a2a958c0534083ed7af916f03288c4feb8ed5
Brian <brian2000bett@gmail.com>
2025-03-13 22:48:11 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,18 @@
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+add();
+
+console.log(add(5, 7));
+const square = (x) => x ** 2;
+square();
+x: 6;
+console.log(square(25));
+
+function divide() {
+  const num1 = 2000;
+  const num2 = 100;
+  return num1 / num2;
+}
+divide();
+
+console.log(divide());
